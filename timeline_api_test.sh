48ec2c2766e914255022e9ee85faa387987699b5
Derek <dragonairex@gmail.com>
2020-03-09 15:47:23 +0800

    Update timeline_api_test.sh

--- timeline_api_test.sh
+++ timeline_api_test.sh
@@ -1,1 +1,2 @@
-curl -H "Accept: application/json" https://redjapan.hk/timeline_webapp/api/index.php
+curl -H "Accept: application/json" https://redjapan.hk/timeline_webapp/api/index.php?screen_name=suzutaro18;
+curl -H "Accept: application/json" https://redjapan.hk/timeline_webapp/api/index.php?screen_name=realdonaldtrump
